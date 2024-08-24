198ec14c8324d69597e37566d362aec90c17a946
Hubert Owusu <hubert7856@gmail.com>
2024-08-24 07:51:51 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ def ghanaweb_form():
   gw_start_date = st.date_input('Start date')
   gw_end_date = st.date_input('End date')
   gw_num = st.text_input('Enter number')
+  return gw_num
 
 
 @st.experimental_dialog('Joy News Online')
@@ -14,15 +15,16 @@ def joynews_form():
   jn_start_date = st.date_input('Start date')
   jn_end_date = st.date_input('End date')
   jn_num = st.text_input('Enter number')
+  return jn_num
 
 col1, col2 = st.columns(2)
 with col1:
   if st.button('Ghanaweb'):
-    ghanaweb_form()
+    gw_num = ghanaweb_form()
     
 with col2:
   if st.button('Joy News'):
-    joynews_form()
+    jn_num = joynews_form()
 
 
 st.write(int(gw_num) + int(jn_num))
