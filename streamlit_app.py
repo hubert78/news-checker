88350b1ea8fec220cad05fd95611d42701510eb5
Hubert Owusu <hubert7856@gmail.com>
2024-08-24 08:24:16 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,28 +2,30 @@ import streamlit as st
 
 st.title('Check News Plagiarism')
 
-@st.dialog('Ghanaweb')
-def ghanaweb_form():
-  gw_start_date = st.date_input('Start date')
-  gw_end_date = st.date_input('End date')
-  gw_num = st.text_input('Enter number')
-  if st.button('Submit'):
-    st.session_state.gw_response = {'start_date': gw_start_date,
-                                    'end_date': gw_end_date,
-                                    'gw_num': gw_num,
-                                   }
+# Ghanaweb form
+# @st.dialog('Ghanaweb')
+with st.form('Ghanaweb'):
+    gw_start_date = st.date_input('Start date')
+    gw_end_date = st.date_input('End date')
+    gw_num = st.text_input('Enter number')
+    gw_submit = st.form_submit_button('Submit')
+    if gw_submit:
+        st.session_state['gw_response'] = {'start_date': gw_start_date,
+                                           'end_date': gw_end_date,
+                                           'gw_num': gw_num,
+                                          }
 
-
-@st.dialog('Joy News Online')
-def joynews_form():
-  jn_start_date = st.date_input('Start date')
-  jn_end_date = st.date_input('End date')
-  jn_num = st.text_input('Enter number')
-  if st.button('Submit'):
-    st.session_state.jn_response = {'start_date': jn_start_date,
-                                    'end_date': jn_end_date,
-                                    'jn_num': jn_num,
-                                   }
+# Joy News form
+with st.form('Joy News Online'):
+    jn_start_date = st.date_input('Start date')
+    jn_end_date = st.date_input('End date')
+    jn_num = st.text_input('Enter number')
+    jn_submit = st.form_submit_button('Submit')
+    if jn_submit:
+        st.session_state['jn_response'] = {'start_date': jn_start_date,
+                                           'end_date': jn_end_date,
+                                           'jn_num': jn_num,
+                                          }
 
 col1, col2 = st.columns(2)
 with col1:
@@ -36,7 +38,8 @@ with col2:
   if jn_button:
     jn_num = joynews_form()
 
+# Display results
 if 'gw_response' in st.session_state and 'jn_response' in st.session_state:
-  gw_num = gw_response['gw_num']
-  jn_num = jn_response['jn_num']
-  st.write(int(gw_num) + int(jn_num))
+    gw_num = st.session_state['gw_response']['gw_num']
+    jn_num = st.session_state['jn_response']['jn_num']
+    st.write(int(gw_num) + int(jn_num))
