d83cdf574ebdcd312f8c4f0ec956b80af4a4c6e2
Hubert Owusu <hubert7856@gmail.com>
2024-08-24 07:45:53 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Check News Plagiarism')
 
-st.write('Hello world!')
+@st.experimental_dialog('Ghanaweb')
+def ghanaweb_form():
+  gw_start_date = st.date_input('Start date')
+  gw_end_date = st.date_input('End date')
+
+
+@st.experimental_dialog('Ghanaweb')
+def joynews_form():
+  gw_start_date = st.date_input('Start date')
+  gw_end_date = st.date_input('End date')
+
+col1, col2 = st.columns(2)
+with col1:
+  if st.button('Ghanaweb'):
+    ghanaweb_form()
+    
+with col2:
+  joynews_form()
