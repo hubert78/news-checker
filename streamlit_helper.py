185901cf0eedb803e69e722796bc17840aeecfa3
Hubert Owusu <hubert7856@gmail.com>
2024-08-27 19:40:59 +0000

    Update streamlit_helper.py

--- streamlit_helper.py
+++ streamlit_helper.py
@@ -14,12 +14,21 @@ def ghanaweb_form():
                     'categories': gw_categories,}
     return ghanaweb_data
 
+def new3_form():
+    categories = ["news", "elections", "business", "showbiz", "sports", "health", "opinion"]
+    new3_start_date = st.date_input('Start date', key='new3_start_date')
+    new3_end_date = st.date_input('End date', key='new3_end_date')
+    new3_categories = st.multiselect('Select news categories', categories, key='new3_categories')
+ 
+    new3_data = {'start_date': new3_start_date,
+                    'end_date': new3_end_date,
+                    'categories': new3_categories,}
+    return new3_data
+
 
 # Joy News form
-#@st.dialog('Joy News')
 def joynews_form():
     categories = ['news', 'business', 'entertainment', 'sports', 'opinion']
-
     jn_start_date = st.date_input('Start date', key='jn_start_date')
     jn_end_date = st.date_input('End date', key='jn_end_date')
     jn_categories = st.multiselect('Select news categories', categories, key='jn_categories')
@@ -31,7 +40,6 @@ def joynews_form():
 
 
 # Modern Ghana form
-#@st.dialog('Modern Ghana')
 def modernghana_form():
     mdg_start_date = st.date_input('Start date', key='mdg_start_date')
     mdg_end_date = st.date_input('End date', key='mdg_end_date')
@@ -43,16 +51,30 @@ def modernghana_form():
     return modernghana_data
 
 
+# Yen Ghana form
+def yenghana_form():
+    categories = ['Ghana', 'Politics', 'Business', 'Entertanment', 'Sports', 'World']
+    yen_start_date = st.date_input('Start date', key='yen_start_date')
+    yen_end_date = st.date_input('End date', key='yen_end_date')
+    yen_categories = st.multiselect('Select news categories', categories, key='yen_categories')
+    yenghana_data = {'start_date': yen_start_date,
+                     'end_date': yen_end_date,
+                     'categories': yen_categories,}
+    return yenghana_data
+
+
 def pop_up(source):
     #try:
     if source == 'Ghanaweb':
         return ghanaweb_form()
+    elif source == '3News':
+        return new3_form()
     elif source == 'MyJoyOnline':
         return joynews_form()
     elif source == 'Modern Ghana':
         return modernghana_form()
-    #except:
-        #st.info('Only one news source enabled. Refresh page')
+    elif source == 'Yen Ghana':
+        return yenghana_form()
 
 
 
